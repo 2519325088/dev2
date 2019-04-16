9ba64229a9d09edb3e3a8eeb4f1181dab0bc9a85
lxl <2519325088@qq.com>
2019-04-16 19:52:30 +0800

    修改数据路错误

--- dev2/demo2/models.py
+++ dev2/demo2/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 
 class World(models.Model):
-    wname=models.CharField(max_lenth=20)
+    wname=models.CharField(max_length=20)
     wtime=models.DateTimeField(auto_now_add=True)
 
 class State(models.Model):
