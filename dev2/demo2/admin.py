a00a2a18234c3403a6479b2bb2580ae399672554
lxl <2519325088@qq.com>
2019-04-16 20:28:58 +0800

    dhanjo可视化

--- dev2/demo2/admin.py
+++ dev2/demo2/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import World,State
+
 # Register your models here.
+admin.site.register(World)
+admin.site.register(State)
+
--- dev2/demo2/models.py
+++ dev2/demo2/models.py
@@ -6,8 +6,13 @@ from django.db import models
 class World(models.Model):
     wname=models.CharField(max_length=20)
     wtime=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.wname
 
 class State(models.Model):
     sname=models.CharField(max_length=30)
     sgender=models.CharField(max_length=20)
     worldid=models.ForeignKey('World',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.sname
